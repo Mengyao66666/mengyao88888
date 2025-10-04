685c98fe3f2b772618dc3e3b52726dc1a8df14a5
Mengyao66666 <my010627@outlook.com>
2025-10-03 22:56:53 -0500

    Add files via upload

--- pycharm projects/npu/conv_npu.py
+++ pycharm projects/npu/conv_npu.py
@@ -89,12 +89,20 @@ def conv2d(X, W, bias):
                     # Allocate a tensor in PSUM
                     # res_psum = nl.zeros((1, 1), dtype=nl.float32, buffer=nl.psum)
 
-                    x_flat = x_tile[:, out_h:out_h + filter_height, out_w:out_w + filter_width].reshape(-1)
-                    # x_flat = x_flat.reshape(in_channels * filter_height * filter_width)
+                    # x_flat = x_tile[:, out_h:out_h + filter_height, out_w:out_w + filter_width].reshape(-1)
+                    # # x_flat = x_flat.reshape(in_channels * filter_height * filter_width)
+                    #
+                    # w_flat = W[out_c].reshape(-1)
+                    #
+                    # out_tile[out_c, out_h, out_w] = nl.dot(w_flat, x_flat) + bias[out_c]
+                    acc = nl.zeros((1,), dtype=nl.float32, buffer=nl.sbuf)
 
-                    w_flat = W[out_c].reshape(-1)
-
-                    out_tile[out_c, out_h, out_w] = nl.dot(w_flat, x_flat) + bias[out_c]
+                    # 直接三重循环做 dot：W[out_c, ic, fh, fw] · X[ic, out_h+fh, out_w+fw]
+                    for ic in nl.affine_range(in_channels):
+                        for fh in nl.affine_range(filter_height):
+                            for fw in nl.affine_range(filter_width):
+                                acc[0] += W[out_c, ic, fh, fw] * x_tile[ic, out_h + fh, out_w + fw]
+                    out_tile[out_c, out_h, out_w] = acc[0] + bias[out_c]
 
         X_out[b, :, :, :] = out_tile
                     # res_psum[out_c] += nl.matmul(w_flat, x_flat)
